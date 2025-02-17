e0ad6bf4e59c30e8fa21bd754b2020d293372860
narendarpaindla <narendarreddypaindla@gmail.com>
2025-02-17 07:02:20 +0000

    DI

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,13 +2,16 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Starting Spring Boot Application...");
-		SpringApplication.run(Main.class, args);
+		ConfigurableApplicationContext context=SpringApplication.run(Main.class, args);
+		Student s1=context.getBean(Student.class);
+		s1.show();
 	}
 
 }
